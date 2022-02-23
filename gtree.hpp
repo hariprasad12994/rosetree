4b8d6727131570af855276cca567e9545a496c78
Hari Prasad <hariprasad12994@gmail.com>
2022-02-23 19:44:10 +0530

    Adding an initial BFS iterator for the tree

--- gtree.hpp
+++ gtree.hpp
@@ -13,3 +13,59 @@ class TreeNode {
     TreeNode(const T& val): value(val), first_child(nullptr), first_sibling(nullptr) {}
     TreeNode(T&& val): value(val), first_child(nullptr), first_sibling(nullptr) {}
 };
+
+
+template <typename Tree>
+struct Iterator {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = typename Tree::value_type;
+    using pointer = value_type*;
+    using reference = value_type;
+
+    using node = typename Tree::node;
+    using node_pointer = node*;
+    using node_reference = node&;
+
+    Iterator(node_pointer ptr): ptr_(ptr) {}
+
+    auto operator*() const -> reference {
+      return ptr_->value;
+    }
+
+    auto operator->() -> node_pointer {
+      return ptr_;
+    }
+
+    auto operator++() -> Iterator& {
+      node_pointer walker = ptr_->first_child;
+      while(walker != nullptr){
+        q.push(walker);
+        walker = walker->first_sibling;
+      }
+      if(!q.empty()){
+        ptr_ = q.front();
+        q.pop();
+      }
+      else{
+        ptr_ = nullptr;
+      }
+      return *this;
+    }
+
+    auto operator++(int) -> Iterator {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    friend bool operator==(const Iterator& a, const Iterator& b) { return a.ptr_ == b.ptr_; }
+    friend bool operator!=(const Iterator& a, const Iterator& b) { return a.ptr_ != b.ptr_; }
+
+  private:
+    std::queue<node_pointer> q;
+    node_pointer ptr_;
+};
+
+
